e92c769cf31604ce5e0c2a146dbbbcdb8fcdb162
avinash gepal <avigepal75@gamil.com>
2021-06-07 23:07:22 +0530

    second commit

--- index2.php
+++ index2.php
@@ -1,5 +1,6 @@
 <?php
 
 echo "my name is avinash and i want to marry after my class end";
+echo "lal bahadur sashtri is a good person and he is very kind for poor people he never abuse any person so no one complaint about him";
 
 ?>
